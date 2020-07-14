3b05e012ca419dd57835b1a622b9c96612f34fd2
courage173 <courageosemwengie@gmail.com>
2020-07-14 06:59:34 +0100

    final push

--- server/app.js
+++ server/app.js
@@ -16,7 +16,12 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 
 app.use(cors())
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 
 app.use(express.static('./client/build'))
